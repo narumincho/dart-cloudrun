6a55c4b51ec9bb7a42c6130de1b4034e414063f6
narumincho <16481886+narumincho@users.noreply.github.com>
2023-01-15 23:17:23 +0000

    run dart create

---
+++ bin/dart_cloudrun.dart
@@ -0,0 +1,5 @@
+import 'package:dart_cloudrun/dart_cloudrun.dart' as dart_cloudrun;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_cloudrun.calculate()}!');
+}
---
+++ test/dart_cloudrun_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_cloudrun/dart_cloudrun.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
