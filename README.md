e49a173cb3c5b1daf86fdfd880c3e728ee0852f5
narumincho <16481886+narumincho@users.noreply.github.com>
2023-01-15 23:46:01 +0000

    サーバーを実装

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 [![Open in Gitpod](https://gitpod.io/button/open-in-gitpod.svg)](https://gitpod.io#https://github.com/narumincho/dart-cloudrun)
 
+[![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://deploy.cloud.run/?git_repo=https://github.com/narumincho/dart-cloudrun)
+
 GitHub でプライベートのメールアドレスを使っている場合のメール設定コマンドの例
 
 ```bash
--- bin/dart_cloudrun.dart
+++ bin/dart_cloudrun.dart
@@ -1,5 +1,38 @@
-import 'package:dart_cloudrun/dart_cloudrun.dart' as dart_cloudrun;
+import 'dart:io';
 
-void main(List<String> arguments) {
-  print('Hello world: ${dart_cloudrun.calculate()}!');
+Future<void> main() async {
+  // If the "PORT" environment variable is set, listen to it. Otherwise, 8080.
+  // https://cloud.google.com/run/docs/reference/container-contract#port
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+
+  final server = await HttpServer.bind(InternetAddress.loopbackIPv4, port);
+
+  print('Serving at http://${server.address.host}:${server.port}');
+
+  server.listen((request) {
+    request.response.headers.contentType =  ContentType.html;
+    request.response.write('''<!doctype html>
+<html lang="ja">
+
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width,initial-scale=1">
+<style>
+body {
+font-size: 48px;
+color: white;
+background-color: black;
+}
+</style>
+</head>
+
+<body>
+<div>Dart で HTTP サーバーを建ててみた!</div>
+<div>現在の時刻 ${DateTime.now().toIso8601String()}</div>
+</body>
+
+</html>
+''');
+    request.response.close();
+  });
 }
